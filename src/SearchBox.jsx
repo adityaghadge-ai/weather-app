0fb23d25872a9124291c22b344ece0e84372834d
Aditya Ghadge <adityaghadge732@gmail.com>
2026-06-10 16:29:42 +0530

    weather api

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -3,7 +3,26 @@ import Button from '@mui/material/Button';
 import './SearchBox.css';
 import {useState} from 'react';
 export default function SearchBox(){
-    const [city,setCity]=useState("");
+     const [city,setCity]=useState("");
+    const API_URL="https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY=import.meta.env.VITE_WEATHER_API_KEY;
+
+    let getWeatherInfo=async()=>{
+        let response=await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse=await response.json();
+        
+
+        let result={
+            temp:jsonResponse.main.temp,
+            tempMin:jsonResponse.main.temp_min,
+            tempMax:jsonResponse.main.temp_max,
+            humidity:jsonResponse.main.humidity,
+            feelsLike:jsonResponse.main.feels_like,
+            weather: jsonResponse.weather[0].description,
+        }
+        console.log(result);
+    }
+   
 
     let handleChange=(evt)=>{
        setCity(evt.target.value);
@@ -13,6 +32,7 @@ export default function SearchBox(){
         evt.preventDefault();
         console.log(city);
         setCity("");
+        getWeatherInfo();
     }
     return (
         <div className="SearchBox"> 
